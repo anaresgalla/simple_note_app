e5d893d3c6909ecddd2ec823810871cfc0da146a
anaresgalla <anaresgalla@gmail.com>
2025-01-27 15:33:20 -0300

    Sets up Delete Method

--- application.rb
+++ application.rb
@@ -17,6 +17,7 @@ module Application
       case choice
       when 'a' then menu.add
       when 'b' then menu.edit
+      when 'c' then menu.delete
       when 'd' then menu.show_all
       end
 
@@ -58,5 +59,20 @@ module Application
       end
     end
 
+    def delete
+      begin
+        @manager.show_all
+        entries = @manager.get_all
+        if entries.length > 0
+          print 'Enter Index Number to Delete: '
+          index = gets.chomp.to_i
+          note = entries.fetch(index-1) #decrease the i by one bc it was increased to show
+          @manager.delete(note)
+          puts 'Entry Deleted'.center(50, '*')
+        end
+      rescue Exception => e
+        puts 'Invalid input'.center(50, '-')
+      end
+    end
   end
 end
