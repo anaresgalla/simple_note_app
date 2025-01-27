9962df65be1a71d106c7860025838caa1c0816c6
anaresgalla <anaresgalla@gmail.com>
2025-01-27 15:13:03 -0300

    Implements Show All method

--- manager.rb
+++ manager.rb
@@ -5,7 +5,19 @@ class Manager
     @database = Database.new
   end
 
-  def store
-    @database.store(note)    
+  def store(note)
+    @database.store(note)
+  end
+
+  def show_all
+    entries = @database.get_all
+    if entries.empty?
+      puts 'No entries found'.center(50, '-')
+    end
+    entries.each_with_index do |item, index|
+      puts (index+1).to_s.center(50, '-')
+      puts item
+      puts ''.center(50, '-')
+    end
   end
 end
--- note.rb
+++ note.rb
@@ -6,4 +6,9 @@ class Note
     @text = text
     @date = Time.now
   end
+
+  #defines a method to show the note instead of its register number
+  def to_s
+    "Date: #{@date.strftime('%Y-%m-%d %H:%N:%S')} \nNote: #@text"
+  end
 end
